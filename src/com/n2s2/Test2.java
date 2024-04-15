ebcd75f27830ff1fce805f5377eb8ba978cee6a1
pradeepsurya <pradeepsurya@live.com>
2024-04-14 23:16:16 -0400

    basics recap

---
+++ src/com/n2s2/Test2.java
@@ -0,0 +1,14 @@
+package com.n2s2;
+
+import com.n2s.Example;
+
+public class Test2 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+		Example ex = new Example();
+		System.out.println(ex);
+	}
+
+}
