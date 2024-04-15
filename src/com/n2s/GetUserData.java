6cd182e904d94791a468c845427ba6e0897fe9e2
pradeepsurya <pradeepsurya@live.com>
2024-04-14 23:15:45 -0400

    basics recap

---
+++ src/com/n2s/GetUserData.java
@@ -0,0 +1,23 @@
+package com.n2s;
+
+import java.util.Scanner;
+
+public class GetUserData {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+		Scanner scan = new Scanner(System.in);
+
+		System.out.println("Enter the name");
+		String name = scan.next();
+
+		System.out.println("Enter the age");
+		int age = scan.nextInt();
+
+		System.out.println("Name = " + name + ", Age = " + age);
+
+		scan.close();
+	}
+
+}
