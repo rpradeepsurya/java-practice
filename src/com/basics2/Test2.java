e2dc7d4c6395c341c0e3235cc681213da5aefec5
pradeepsurya <pradeepsurya@live.com>
2024-04-15 07:43:48 -0400

    update group id

---
+++ src/com/basics2/Test2.java
@@ -0,0 +1,14 @@
+package com.basics2;
+
+import com.basics.Example;
+
+public class Test2 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+		Example ex = new Example();
+		System.out.println(ex);
+	}
+
+}
