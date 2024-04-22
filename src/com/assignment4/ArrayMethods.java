1c748cb6b577e4664e275430041cec644c0fc155
pradeepsurya <pradeepsurya@live.com>
2024-04-21 23:57:59 -0400

    Add array exercises

--- src/com/assignment4/ArrayMethods.java
+++ src/com/assignment4/ArrayMethods.java
@@ -30,7 +30,47 @@ public class ArrayMethods {
 		catch(ArrayIndexOutOfBoundsException e) {
 			System.out.println("Array Index out of bound exception: " + e.getMessage());
 		}
+		
+		// 3. Missing number in an array
+		
+		
+		// 4. Search element in an array
+		int[] array = {5, 1, 4, 2, 8, 3};
+		int target = 8;
+		
+		int index = linearSearch(array, target);
+		
+		if (index != -1) System.out.println("Element found at index: " + index);
+		else System.out.println("Element not found in the array");
+		
+		
+		// 5. Intersection between two arrays
+		
+		
+		// 6. Sum of all the values in a given array
+		
+		int[] array = {10, 43, 27, 98, 75, 59, 191};
+		int sum = 0;
+		
+		for (int num: array) sum += num;
+		
+		System.out.println("Sum of array: " + sum);
+		
+		
+		// 7. check if array contains value or not
+		
+		
+		
 
 	}
+	
+	public static int linearSearch(int[] array, int target) {
+        for (int i = 0; i < array.length; i++) {
+            if (array[i] == target) {
+                return i;
+            }
+        }
+        return -1;
+    }
 
 }
