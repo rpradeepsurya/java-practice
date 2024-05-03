fc978728beaefa6c05035fd0b8f71b13afb1a01b
pradeepsurya <pradeepsurya@live.com>
2024-05-03 02:22:17 -0400

    Add methods to insert update delete view entries

---
+++ src/n2s/ehc/pradeepsurya/UserOptions.java
@@ -0,0 +1,74 @@
+package n2s.ehc.pradeepsurya;
+
+import java.util.Hashtable;
+import java.util.Scanner;
+
+public class UserOptions {
+
+	private Hashtable<Integer, UserBean> hashTable = new Hashtable<>();
+	private Scanner scan = new Scanner(System.in);
+
+	UserOptions() {
+
+	}
+
+	public void insert() throws InvalidUserId {
+
+		System.out.print("Enter UserId: ");
+		int userId = Integer.parseInt(scan.nextLine());
+		System.out.println();
+		if (hashTable.containsKey(userId))
+			throw new InvalidUserId("User Id already exist. Try different");
+
+		
+		System.out.print("Enter Name: ");
+		String userName = scan.nextLine();
+		hashTable.put(userId, new UserBean(userId, userName));
+		System.out.println("\nYour Insert success!!!");
+
+	}
+
+	public void update() throws UserIdNotFound {
+
+		System.out.print("Enter UserId: ");
+		int userId = Integer.parseInt(scan.nextLine());
+
+		if (!hashTable.containsKey(userId))
+			throw new UserIdNotFound("User Id not available. Try different");
+
+		System.out.print("\nEnter Name: ");
+		String userName = scan.nextLine();
+		hashTable.put(userId, new UserBean(userId, userName));
+		System.out.println("Update success!!!");
+
+	}
+
+	public void view() {
+
+		System.out.println("S.No \t Id \t Name");
+		System.out.println("--------------------------");
+		int count = 1;
+		for (UserBean user : hashTable.values()) {
+			System.out.println(count++ + "\t " + user.getUserId() + "\t " + user.getUserName());
+		}
+
+	}
+
+	public void delete() throws UserIdNotFound {
+
+		System.out.print("\nEnter UserId: ");
+		int userId = Integer.parseInt(scan.nextLine());
+
+		if (!hashTable.containsKey(userId))
+			throw new UserIdNotFound("User Id not available. Try different");
+
+		hashTable.remove(userId);
+		System.out.println("Delete success!!!");
+
+	}
+	
+	public void closeScan() {
+		scan.close();
+	}
+
+}
